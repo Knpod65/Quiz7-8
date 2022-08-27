1a104c03516705866f727d515d22ac2ca946863d
Atikarn Saengwilai <Atikarn_sa@cmu.ac.th>
2022-08-27 13:31:17 +0700

    improve on that's comment what you send to me
    and you will read describe on line by line

--- Quiz8.py
+++ Quiz8.py
@@ -1,46 +1,18 @@
-def CM(m): #CM(m) is assigned for the reason that show how to begin and stop matrix.
-    def CD(i,j,m): #CD's function is defined because arranged matrix.
-        def IF(i,j,m): #function OF 'IF' is determined by the condition.
-            h=""
-            if i<j and j != m:
-                h+=" 0"
-            elif i==j or j==m:
-                h+=" 1"
-            else:
-                h+="-1"
-            return h
-        
-        g=""
-        if j != m:
-            g+=IF(i,j,m)
-            g+=". "
-        else:
-            g+=IF(i,j,m)
-            g+="."
-        return g
-    
-    def FR(i,m): #FR's function is imposed about what function can run on column and row together.
-        z=""
-        if i != m:
-            for j in range(1,m+1):
-                z+=CD(i,j,m)
-            z+="]\n"
-        else:
-            for j in range(1,m+1):
-                z+=CD(i,j,m)
-            z+="]"
-        return z
-    
-    c="["
-    for i in range(1,m+1):
-        if i ==1:
-            c+="["
-            c+=FR(i,m)
-        else:
-            c+=" ["
-            c+=FR(i,m)
-    c+= "]"
-    return c
-
-m=int(input("Put your order to make the square matrix (m): "))
-print(CM(m)) #this show result in this quiz.
+import numpy as np #import indispensable function that will recall in this quiz name numpy
+def q(m, matrix): #define the function that I will use in this quiz.
+    for i in range(m): #this is for-loop about i that means column on this matrix and it repeats until it is m (column) 
+        a =[]  #when it starts on next row, it will begin and reset list of a is empty
+        for j in range(m): #this is for-loop about j that means column on this matrix and it repeats until it is m (row)
+            if i<j and j!=m: #condition of this qiuz
+                a.append(0) #append 0 when this condition is TRUE in list of a
+            elif i==j or j==m: #condition of this qiuz
+                a.append(1) #append 1 when this condition is TRUE in list of a
+            else: #condition of this qiuz
+                a.append(-1) #append -1 when all previous conditions is FALSE in list of a
+        matrix.append(a) #append column in matrix
+matrix=[] #begin empty list to recieve every list that will produce list of list
+m=int(input("Put your order to make the square matrix (m): ")) #input value what I use to make the square matrix
+q(m,matrix) #this recall the function that relate between empty matrix and count of row or column: n(row) equals n(column)
+matrix=np.array(matrix).reshape(R,R) # when array function works, list will be aggregated about what's column and row by matrix's list
+# we will reshape it on general the square matrix
+print(matrix)
